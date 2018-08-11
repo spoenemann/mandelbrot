0fed5621952e48d234a2042de65ff553ca905c48
Miro Spönemann <miro.spoenemann@typefox.io>
2018-08-11 11:54:02 +0200

    Added license header

--- java/src/main/java/io/github/spoenemann/mandelbrot/Pixel.java
+++ java/src/main/java/io/github/spoenemann/mandelbrot/Pixel.java
@@ -1,6 +1,10 @@
-/**
- * Funky Mandelbrot Application (c) 2014 by Miro
- */
+/*********************************************************************
+* Copyright (c) 2014 Miro Spönemann
+*
+* This program and the accompanying materials are made
+* available under the terms of the Eclipse Public License 2.0
+* which is available at https://www.eclipse.org/legal/epl-2.0/
+**********************************************************************/
 package io.github.spoenemann.mandelbrot;
 
 import java.util.LinkedList;
